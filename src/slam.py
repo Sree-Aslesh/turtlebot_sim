e2f53e811a5f088949926b809898a32eb1d0050e
Sree Aslesh <sree.aslesh@gmail.com>
2020-08-20 23:08:12 -0500

    local path planning implemented
    local path planning implementation is done using DWA by checking the path of the robot in multiple variation of angular velocity

--- src/slam.py
+++ src/slam.py
@@ -3,6 +3,7 @@
 import rospy
 import tf
 import heapq
+import math
 from geometry_msgs.msg import Twist,PoseStamped,Pose
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import MapMetaData,OccupancyGrid,Path
@@ -15,6 +16,7 @@ turn_speed = 2
 turn_rad = 1.5708
 velocity_publisher = None
 globalPathPublisher = None
+localPathPublisher = None
 
 vel_msg = Twist()
 
@@ -31,6 +33,7 @@ goal = PoseStamped()
 poseReceived = False
 listener = None
 goal = None
+map_holder = None
 
 
 map_metadata = MapMetaData()
@@ -96,11 +99,45 @@ def shortestPath(curr_x,curr_y,des_x,des_y,map,pathList,visited):
     return []
 
 
+def seq(start,stop,step):
+    temp =[]
+    while(start<=stop):
+        temp.append(start)
+        start = start + step
+    return temp
+
+def runTurtle(path):
+    global vel_msg
+
+    vel_msg.linear.x = 0
+    vel_msg.linear.y = 0
+    vel_msg.linear.z = 0
+    vel_msg.angular.x = 0
+    vel_msg.angular.y = 0
+    vel_msg.angular.z = 0
+
+    (trans,rot) = listener.lookupTransform('map', 'turtlebot/create::base', rospy.Time(0))
+    linearVel = 0.3
+
+    for angVel in seq(-1,1,0.2):
+        localPath = Path()
+        localPath.header.frame_id="map"
+        xval = trans[0]
+        yval = trans[1]
+        for secs in seq(0,1,0.2):
+            xval = xval + linearVel*secs + linearVel*math.cos(angVel*secs)
+            yval = yval + linearVel*math.sin(angVel*secs)
+            localPath.poses.append(createPose(x=xval,y=yval))
+        localPathPublisher.publish(localPath)
+        rospy.sleep(0.1)
+
+
 
 
 def getMap(map):
     print("[getMap] - Received map")
-
+    global map_holder
+    map_holder = map
     if(poseReceived!=True):
         return
     
@@ -125,13 +162,14 @@ def getMap(map):
     visited = [0]*(4000*4000)
     pathList = []
     pathList = shortestPath(grid_curr_x,grid_curr_y,grid_des_x,grid_des_y,map,pathList,visited)
-    print(pathList)
+    # print(pathList)
 
     path = Path()
     path.header.frame_id="map"
     for i,val in enumerate(pathList):
         path.poses.append(createPose(x=val[0]*map_metadata.resolution+map_metadata.origin.position.x,y=val[1]*map_metadata.resolution+map_metadata.origin.position.y))
     globalPathPublisher.publish(path)
+    runTurtle(path)
     
 
 
@@ -140,11 +178,12 @@ def goalPose(goal_input):
     print("[goalPose] - Received goal location .. Starting SLAM")
     goal = goal_input
     poseReceived = True
+    getMap(map_holder)
     # print(goal.pose.position.x)
 
 
 def laser_callback(laser):
-    #print("reading laser scan data")
+    # print("reading laser scan data")
     global turn,turn_speed,velocity_publisher,vel_msg
     # print("in laser call back" + str(poseReceived))
     if(poseReceived==True):
@@ -156,13 +195,13 @@ def laser_callback(laser):
         vel_msg.angular.y = 0
         vel_msg.angular.z = 0
         #print(type(laser.ranges[-1]))
-        if(float("inf") in laser.ranges): 
-            # print("count is " + str(laser.ranges[310:330].count(float("inf"))))
-            if(laser.ranges[310:330].count(float("inf")) == 20):
-                vel_msg.linear.x = 0.5
-            else:
-                vel_msg.linear.x = 0
-                vel_msg.angular.z = 0.1
+        # if(float("inf") in laser.ranges): 
+        #     # print("count is " + str(laser.ranges[310:330].count(float("inf"))))
+        #     if(laser.ranges[310:330].count(float("inf")) == 20):
+        #         vel_msg.linear.x = 0.5
+        #     else:
+        #         vel_msg.linear.x = 0
+        #         vel_msg.angular.z = 0.1
         # print("publishing message")
         velocity_publisher.publish(vel_msg)
 
@@ -170,18 +209,21 @@ def laser_callback(laser):
 
 def move():
     # Starts a new node
-    global turn,turn_speed,velocity_publisher,vel_msg,listener,globalPathPublisher
+    global turn,turn_speed,velocity_publisher,vel_msg,listener,globalPathPublisher,localPathPublisher
     rospy.init_node('robot_runner')
     print("[move] - Initialized node 'robot_runner'")
 
     listener = tf.TransformListener()
-    rospy.sleep(10)
+    rospy.sleep(5)
 
     velocity_publisher = rospy.Publisher('/turtlebot/cmd_vel', Twist, queue_size=10)
     print("[move] - Initialized publisher for topic '/turtlebot/cmd_vel'")
 
     globalPathPublisher = rospy.Publisher('/globalPath',Path,queue_size=10)
     print("[move] - Initialized publisher for topic '/globalPath'")
+
+    localPathPublisher = rospy.Publisher('/localPath',Path,queue_size=10)
+    print("[move] - Initialized publisher for topic '/localPath'")
 
     rospy.Subscriber('/hokuyo_laser', LaserScan, laser_callback )
     print("[move] - Initialized subscriber for topic '/hokuyo_laser'")
